a412358632ab89c1f1f5fd791a74c009f0543d1f
Denis Carriere <carriere.denis@gmail.com>
2025-06-24 00:27:37 -0400

    update tests

--- tests/pumpfun.rs
+++ tests/pumpfun.rs
@@ -6,7 +6,7 @@ mod tests {
     #[test]
     fn unpack_trade_event() {
         // https://solscan.io/tx/sK44CDg4qzi9jvTgA32dCTNh6Y3CgXki2kj9XtpaXRr83BipzpWPjnENzJR3TjLegAfDfPDG5Z8GZDkbrXDQk3w
-        let bytes = hex!("e445a52e51cb9a1dbddb7fd34ee661ee1506533f60e64c4528916a7b404296ac72a1b1b65e817505f94b02b46d1969be46b1f416000000000076c04f9a040000013cd2f118afae78c0d0a91f649ff048b4a16749e82caac521286d90a6085cdd1026b2466800000000e7329a910b0000009d11034a374d0200e7867695040000009d79f0fda54e0100ad11e6a4fc2944a4fa8251bef815426e1bfb28c6b6646677607c6ad9f566a6465f000000000000001ed4370000000000deed67d04e125b1a2d7c6933fec6c7b08bce5763a576c88dcd36697d38298183050000000000000038f0020000000000").to_vec();
+        let bytes = hex!("e445a52e51cb9a1dbddb7fd34ee661ee1506533f60e64c4528916a7b404296ac72a1b1b65e817505f94b02b46d1969be46b1f416000000000076c04f9a040000013cd2f118afae78c0d0a91f649ff048b4a16749e82caac521286d90a6085cdd1026b2466800000000e7329a910b0000009d11034a374d0200e7867695040000009d79f0fda54e0100ad11e6a4fc2944a4fa8251bef815426e1bfb28c6b6646677607c6ad9f566a6465f000000000000001ed4370000000000deed67d04e125b1a2d7c6933fec6c7b08bce5763a576c88dcd36697d38298183050000000000000038f0020000000000");
 
         match pumpfun::events::unpack(&bytes).expect("decode PumpFunEvent") {
             pumpfun::events::PumpFunEvent::Trade(event) => {
@@ -39,7 +39,7 @@ mod tests {
     #[test]
     fn unpack_buy_instruction() {
         // https://solscan.io/tx/sK44CDg4qzi9jvTgA32dCTNh6Y3CgXki2kj9XtpaXRr83BipzpWPjnENzJR3TjLegAfDfPDG5Z8GZDkbrXDQk3w
-        let bytes = hex!("66063d1201daebea0076c04f9a040000960d301700000000").to_vec();
+        let bytes = hex!("66063d1201daebea0076c04f9a040000960d301700000000");
 
         match pumpfun::instructions::unpack(&bytes).expect("decode PumpFunInstruction") {
             pumpfun::instructions::PumpFunInstruction::Buy(event) => {
@@ -58,7 +58,7 @@ mod tests {
     #[test]
     fn unpack_create_instruction() {
         // https://solscan.io/tx/XzPzYUbKHHrG6gyKciKW2yHWqzjF1cbuiJzN6bwYQy3oQLwaFwc5ErKpMc1QHr9BLxuLzfqKaSosetgXgREyqc9
-        let bytes = hex!("181ec828051c077704000000474f424f04000000474f424f5000000068747470733a2f2f697066732e696f2f697066732f6261666b7265696470357362746f346d76757472367463646b713574763262357a70336f727a706264646d767a3262786279747463336b6969326d77b855e4b353e62f466ee300057240ca099fc08b16e82dc80ab252b6d0c3b8bc").to_vec();
+        let bytes = hex!("181ec828051c077704000000474f424f04000000474f424f5000000068747470733a2f2f697066732e696f2f697066732f6261666b7265696470357362746f346d76757472367463646b713574763262357a70336f727a706264646d767a3262786279747463336b6969326d77b855e4b353e62f466ee300057240ca099fc08b16e82dc80ab252b6d0c3b8bc");
 
         match pumpfun::instructions::unpack(&bytes).expect("decode PumpFunInstruction") {
             pumpfun::instructions::PumpFunInstruction::Create(event) => {
