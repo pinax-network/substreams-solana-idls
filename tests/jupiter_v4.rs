1fdf6b31df5ca0ce61b56162decca983fa835924
Denis Carriere <carriere.denis@gmail.com>
2025-07-06 16:07:06 -0400

    Rename to anchor_self_cpi

--- tests/jupiter_v4.rs
+++ tests/jupiter_v4.rs
@@ -8,11 +8,11 @@ mod tests {
         // https://solscan.io/tx/5xQncr8APuvFEzZVnBDJf7BKdFB5MRQ4nFpmCPZxUgqriAn4gLUHhRe6hXwiEsqxWCmATHQraV7Rx6E7gci2CAP
         let bytes = hex!("516ce3becdd00ac47e54771a57a6f14ca9e402d54aee45f7378aca365c7b169a7ec83f5182b298f0069b8857feab8184fb687f634618c035dac439dc1aeb3b5598a0f0000000000103e727000000000006a1ec5bd82ad9c032a9f7d466ba2c728b0ef36a8b773ed219d69650d3472bd665ba6d3901000000");
 
-        match jupiter::v4::events::unpack(&bytes).expect("decode jupiterEvent") {
-            jupiter::v4::events::JupiterV4Event::Swap(event) => {
+        match jupiter::v4::anchor_self_cpi::unpack(&bytes).expect("decode jupiterEvent") {
+            jupiter::v4::anchor_self_cpi::JupiterV4Event::Swap(event) => {
                 assert_eq!(
                     event,
-                    jupiter::v4::events::SwapEvent {
+                    jupiter::v4::anchor_self_cpi::SwapEvent {
                         amm: "9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP".parse().expect("parse Pubkey"),
                         input_mint: "So11111111111111111111111111111111111111112".parse().expect("parse Pubkey"),
                         input_amount: 2615043,
--- tests/jupiter_v6.rs
+++ tests/jupiter_v6.rs
@@ -8,11 +8,11 @@ mod tests {
         // https://solscan.io/tx/5xQncr8APuvFEzZVnBDJf7BKdFB5MRQ4nFpmCPZxUgqriAn4gLUHhRe6hXwiEsqxWCmATHQraV7Rx6E7gci2CAP
         let bytes = hex!("e445a52e51cb9a1d40c6cde8260871e20c14defc825ec67694250818bb654065f4298d3156d571b4d4f8090c18e9a863fd51dc4c913e7ca8e73cd3cbb45769ac41aaade3696540dcf482a5fe57383a8f28cac2eb06000000069b8857feab8184fb687f634618c035dac439dc1aeb3b5598a0f000000000019bdc520300000000");
 
-        match jupiter::v6::events::unpack(&bytes).expect("decode jupiterEvent") {
-            jupiter::v6::events::JupiterV6Event::Swap(event) => {
+        match jupiter::v6::anchor_self_cpi::unpack(&bytes).expect("decode jupiterEvent") {
+            jupiter::v6::anchor_self_cpi::JupiterV6Event::Swap(event) => {
                 assert_eq!(
                     event,
-                    jupiter::v6::events::SwapEvent {
+                    jupiter::v6::anchor_self_cpi::SwapEvent {
                         amm: "pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA".parse().expect("parse Pubkey"),
                         input_mint: "J3rYdme789g1zAysfbH9oP4zjagvfVM2PX7KJgFDpump".parse().expect("parse Pubkey"),
                         input_amount: 29725215272,
@@ -30,11 +30,11 @@ mod tests {
         // https://solscan.io/tx/3tNX6MzVEnsDCJWSnM5xqBQNWXh22T3q2PkF1hys35P9kpxVuut6chgPbnFs5Cyp9ygU9EidQgvqbuak26u1bmQM
         let bytes = hex!("e445a52e51cb9a1d494f4e7fb8d50ddc71337d91df2be75ff1c1ce88c1f5f0293ce7ee4562f6ee3e045fa07faad311dd069b8857feab8184fb687f634618c035dac439dc1aeb3b5598a0f0000000000108b5000000000000");
 
-        match jupiter::v6::events::unpack(&bytes).expect("decode jupiterEvent") {
-            jupiter::v6::events::JupiterV6Event::Fee(event) => {
+        match jupiter::v6::anchor_self_cpi::unpack(&bytes).expect("decode jupiterEvent") {
+            jupiter::v6::anchor_self_cpi::JupiterV6Event::Fee(event) => {
                 assert_eq!(
                     event,
-                    jupiter::v6::events::FeeEvent {
+                    jupiter::v6::anchor_self_cpi::FeeEvent {
                         account: "8ctcHN52LY21FEipCjr1MVWtoZa1irJQTPyAaTj72h7S".parse().expect("parse Pubkey"),
                         mint: "So11111111111111111111111111111111111111112".parse().expect("parse Pubkey"),
                         amount: 46344,
