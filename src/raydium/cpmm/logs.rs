3a65946db1533214a598a53e14a7c879c3a2d375
Denis Carriere <carriere.denis@gmail.com>
2025-08-30 21:57:41 +0200

    remove extras

--- src/raydium/cpmm/logs.rs
+++
@@ -1,3 +0,0 @@
-//! Raydium CPMM logs re-export.
-
-pub use super::anchor_self_cpi::{unpack, LpChangeEvent, RaydiumCpmmEvent as RaydiumCpmmLog, SwapEvent, LP_CHANGE_EVENT, SWAP_EVENT};
--- src/raydium/cpmm/mod.rs
+++ src/raydium/cpmm/mod.rs
@@ -1,8 +1,6 @@
 use substreams_solana::b58;
 
-pub mod anchor_self_cpi;
 pub mod instructions;
-pub mod logs;
 
 /// Raydium Concentrated Pool Market Maker program
 ///
