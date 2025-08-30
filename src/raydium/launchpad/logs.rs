ac5096008bdf64ed5c6634f6b7df8bea269d53f0
Denis Carriere <carriere.denis@gmail.com>
2025-08-30 21:58:50 +0200

    remove extras

--- src/raydium/launchpad/logs.rs
+++
@@ -1,3 +0,0 @@
-//! Raydium Launchpad logs re-export.
-
-pub use super::anchor_self_cpi::{unpack, RaydiumLaunchpadEvent as RaydiumLaunchpadLog, TradeEvent, TRADE_EVENT};
--- src/raydium/launchpad/mod.rs
+++ src/raydium/launchpad/mod.rs
@@ -1,8 +1,6 @@
 use substreams_solana::b58;
 
-pub mod anchor_self_cpi;
 pub mod instructions;
-pub mod logs;
 
 /// Raydium Launchpad program
 ///
