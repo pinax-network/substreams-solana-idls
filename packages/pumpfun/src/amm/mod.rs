02e922e53b138d82b167981ebf7305743cdf3116
Denis Carriere <carriere.denis@gmail.com>
2025-09-13 19:40:32 -0400

    Add comments

--- packages/pumpfun/src/amm/mod.rs
+++ packages/pumpfun/src/amm/mod.rs
@@ -4,7 +4,7 @@ pub mod events;
 pub mod instructions;
 pub mod logs;
 
-// -----------------------------------------------------------------------------
-// Program ID
-// -----------------------------------------------------------------------------
+/// PumpFun AMM program
+///
+/// https://solscan.io/account/pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA
 pub const PROGRAM_ID: [u8; 32] = b58!("pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA");
--- packages/pumpfun/src/bonding_curve/mod.rs
+++ packages/pumpfun/src/bonding_curve/mod.rs
@@ -3,7 +3,7 @@ pub mod accounts;
 pub mod events;
 pub mod instructions;
 
-// -----------------------------------------------------------------------------
-// Program ID
-// -----------------------------------------------------------------------------
+/// PumpFun Bonding Curve
+///
+/// https://solscan.io/account/6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P
 pub const PROGRAM_ID: [u8; 32] = b58!("6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P");
