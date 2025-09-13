f89ee0e7c520d2ac1ace9d8972979594da90bc5e
Denis Carriere <carriere.denis@gmail.com>
2025-09-13 15:24:30 -0400

    refactor phoenix

--- packages/phoenix/src/lib.rs
+++ packages/phoenix/src/lib.rs
@@ -1,1 +1,11 @@
-// Phoenix crate placeholder
+extern crate idls_common;
+use substreams_solana::b58;
+
+pub mod accounts;
+pub mod events;
+pub mod instructions;
+
+/// Phoenix program
+///
+/// https://solscan.io/account/PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY
+pub const PROGRAM_ID: [u8; 32] = b58!("PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY");
